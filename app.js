251ff4d3d291e297635088f30d6bdce70b1af9b2
Favour Ori <orifavour2000@gmail.com>
2020-01-09 14:35:04 +0200

    minor fix

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.post("/", (req, res) => {
   console.log(req.body);
   switch (text) {
     case "":
-      response = `CON Welcome to Bank of Favour. What do you want to to? \n 1. My account balance \n 1. Check phone number`;
+      response = `CON Welcome to Bank of Favour. What do you want to to? \n 1. My account balance \n 2. Check phone number`;
       break;
     case "1":
       response = "Your account balance is $100";
